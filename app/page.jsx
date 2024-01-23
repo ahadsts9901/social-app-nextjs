2b4d11c846a2d16b06552b67e62ff32ab125f7d9
Muhammad Ahad <120649081+ahadsts9901@users.noreply.github.com>
2024-01-23 14:15:30 +0500

    navbar

--- app/page.jsx
+++ app/page.jsx
@@ -3,14 +3,14 @@
 import React from 'react'
 import { v2Theme } from './Mui/client.mjs';
 import { ThemeProvider } from '@emotion/react';
-import LabelBottomNavigation from './Mui/components/LabelBottomNavigation';
+import MiniDrawer from './Mui/components/MiniDrawer';
 
 const page = () => {
   return (
     <>
       <>
         <ThemeProvider theme={v2Theme}>
-          <LabelBottomNavigation />
+          <MiniDrawer />
         </ThemeProvider>
       </>
     </>
